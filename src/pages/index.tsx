6c68ea86e740a57da5c21c2eee95aa8f5c2fea4f
Merlinthewizord <142792777+Merlinthewizord@users.noreply.github.com>
2025-11-09 04:15:44 -0600

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -68,20 +68,20 @@ export default function HomePage() {
           <div className="whitespace-nowrap grid grid-cols-2 grid-rows-2 gap-2 mt-5 md:flex md:flex-col md:mt-0 md:justify-start">
             <button
               onClick={() =>
-                window.open("https://github.com/BankkRoll/Gamba-V2-Next.js")
+                window.open("world.trenchdotworld.com")
               }
               className="rounded-lg p-3 bg-white hover:bg-gray-200 hover:-translate-y-0.5 transform text-black transition-all duration-200 ease-in-out cursor-pointer shadow-lg hover:shadow-xl"
             >
               Join Trench World
             </button>
             <button
-              onClick={() => window.open("https://world.trenchdotworld.com")}
+              onClick={() => window.open("https://trenchdotworld.com")}
               className="rounded-lg p-3 bg-white hover:bg-gray-200 hover:-translate-y-0.5 transform text-black transition-all duration-200 ease-in-out cursor-pointer shadow-lg hover:shadow-xl"
             >
               Trench World Launchpad
             </button>
             <button
-              onClick={() => window.open("https://trenchdotworld.com")}
+              onClick={() => window.open("https://t.me/trenchworldonline.com")}
               className="rounded-lg p-3 bg-white hover:bg-gray-200 hover:-translate-y-0.5 transform text-black transition-all duration-200 ease-in-out cursor-pointer shadow-lg hover:shadow-xl"
             >
               Our Telegram
